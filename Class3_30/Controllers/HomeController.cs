957560f03d95ec87246eda220809130949135fcf
Sarala Retter <saralaretter@gmail.com>
2020-04-01 21:20:23 -0400

    Add project files.

---
+++ Class3_30/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Class3_30.Models;
+
+namespace Class3_30.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Results(string text)
+        {
+            Dictionary<char, int> alphabet = new Dictionary<char, int>();
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char c in abc)
+            {
+                alphabet[c] = 0;
+            }
+            string searchText = text.ToLower();
+            foreach (char c in searchText)
+            {
+                if (alphabet.ContainsKey(c))
+                {
+                    alphabet[c]++;
+                }
+            }
+            ResultsViewModel vm = new ResultsViewModel
+            {
+                Abc = "abcdefghijklmnopqrstuvwxyz",
+                Alphabet = alphabet,
+                Text = text
+            };
+            
+            return View(vm);
+        }
+
+    }
+}
---
+++ Class3_30/Models/ResultsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class3_30.Models
+{
+    public class ResultsViewModel
+    {
+        public string Abc { get; set; }
+        public Dictionary<char, int> Alphabet { get; set; }
+        public string Text { get; set; }
+    }
+}
